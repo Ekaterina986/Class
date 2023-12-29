38aca7abc74fd4b35934ae94e0c1778d7b30abdb
Ekaterina986 <kev986@mail.ru>
2023-12-29 19:01:45 +0300

    task 2

--- src/js/Character.js
+++ src/js/Character.js
@@ -13,5 +13,24 @@ export default class Character {
         this.level = 1;
         this.attack = null;
         this.defence = null;
-      }
+    }
+    
+    levelUp() {
+        if (this.health > 0) {
+            this.level = this.level + 1;
+            this.attack = this.attack + (0,2*this.attack);
+            this.defence = this.defence + (0,2*this.defence);
+            this.health = 100;
+        } else {
+            throw new Error('invalid healths');
+        }
+    }
+
+    damage(points) {
+        if (this.health >= 0) {
+            this.health -= points * (1 - this.defence / 100);
+        } else {
+            throw new Error('invalid healths');
+        }
+    }
 }
--- src/js/__tests__/Character.test.js
+++ src/js/__tests__/Character.test.js
@@ -19,6 +19,52 @@ test('create copy', () => {
     }).toThrow();
 
     expect(() => {
-    const character = new Character( 'aaa', 'Bowerman1');
+        const character = new Character( 'aaa', 'Bowerman1');
+    }).toThrow();
+});
+
+test('levelUp', () => {
+    const character = new Character( 'abc', 'Bowerman');
+    character.health = 50;
+
+    const correct = {
+        type: character.type,
+        name: character.name,
+        attack: character.attack + (0,2*character.attack),
+        defence: character.defence + (0,2*character.defence),
+        level: 2,
+        health: 100,
+    }
+    character.levelUp();
+
+    expect(character).toEqual(correct);
+    
+    
+    character.health = 0;
+    expect(() => {
+        character.levelUp();
+    }).toThrow();
+    
+});
+
+test('damage', () => {
+    const character = new Character( 'abc', 'Bowerman');
+    
+    const points = 10;
+    const correct = {
+        type: character.type,
+        name: character.name,
+        attack: character.attack,
+        defence: character.defence,
+        level: 1,
+        health: character.health - points * (1 - character.defence / 100),
+    }
+    character.damage(points);
+
+    expect (character).toEqual(correct);
+
+    character.health = -1;
+    expect(() => {
+        character.damage();
     }).toThrow();
 });
